a2c35d82b54c5d35987eecea21fbeb540abfaba3
wonnor-pro <wonnor.pro@gmail.com>
2021-01-24 16:16:45 +0000

    merge graph feature

--- app/src/main/java/com/txbb/nfctimeapp/frontend/graphs/GraphFragment.java
+++ app/src/main/java/com/txbb/nfctimeapp/frontend/graphs/GraphFragment.java
@@ -13,6 +13,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.txbb.nfctimeapp.R;
 import com.txbb.nfctimeapp.TagProperties;
 import com.txbb.nfctimeapp.backend.Actor;
@@ -42,35 +52,76 @@ public class GraphFragment extends Fragment implements Actor {
 ////        mViewModel = new ViewModelProvider(this).get(GraphViewModel.class);
 ////        // TODO: Use the ViewModel
 //
-//        RadarChart radarChart = getActivity().findViewById(R.id.radarChart);
-//        System.out.println(getActivity());
-//        System.out.println(radarChart);
-//
-//        ArrayList<RadarEntry> userTraits = new ArrayList<>();
-//
-//        userTraits.add(new RadarEntry(4.4f));
-//        userTraits.add(new RadarEntry(5f));
-//        userTraits.add(new RadarEntry(4.3f));
-//        userTraits.add(new RadarEntry(4f));
-//        userTraits.add(new RadarEntry(3.8f));
-//
-//        RadarDataSet radarDataUserTraits = new RadarDataSet(userTraits, "User");
-//        radarDataUserTraits.setColor(Color.RED);
-//        radarDataUserTraits.setLineWidth(2f);
-//        radarDataUserTraits.setValueTextColor(Color.RED);
-//        radarDataUserTraits.setValueTextSize(14f);
-//
-//        RadarData radarData = new RadarData();
-//        radarData.addDataSet(radarDataUserTraits);
-//
-//        String[] labels = {"Power", "Consistency", "Diversity", "Diversity", "Tenacity", "Balance"};
-//
-//        XAxis xAxis = radarChart.getXAxis();
-//        xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
-//
-//        System.out.println(radarData);
-//        radarChart.setData(radarData);
 
+        RadarChart radarChart = getActivity().findViewById(R.id.radarChart);
+
+        ArrayList<RadarEntry> userTraitsThisWeek = new ArrayList<>();
+
+        userTraitsThisWeek.add(new RadarEntry(4.4f));
+        userTraitsThisWeek.add(new RadarEntry(5f));
+        userTraitsThisWeek.add(new RadarEntry(4.3f));
+        userTraitsThisWeek.add(new RadarEntry(4f));
+        userTraitsThisWeek.add(new RadarEntry(3.8f));
+
+        RadarDataSet radarDataUserTraits = new RadarDataSet(userTraitsThisWeek, "Last Week");
+        radarDataUserTraits.setColor(Color.rgb(244,67,54));
+        radarDataUserTraits.setLineWidth(1.5f);
+        radarDataUserTraits.setValueTextSize(0f);
+
+        ArrayList<RadarEntry> userTraitsLastWeek = new ArrayList<>();
+        userTraitsLastWeek.add(new RadarEntry(5.0f));
+        userTraitsLastWeek.add(new RadarEntry(4.6f));
+        userTraitsLastWeek.add(new RadarEntry(4.6f));
+        userTraitsLastWeek.add(new RadarEntry(4.8f));
+        userTraitsLastWeek.add(new RadarEntry(4.5f));
+
+        RadarDataSet radarDataUserTraitsLastWeek = new RadarDataSet(userTraitsLastWeek, "This Week");
+        radarDataUserTraitsLastWeek.setColor(Color.rgb(0,150,136));
+        radarDataUserTraitsLastWeek.setLineWidth(1.5f);
+        radarDataUserTraitsLastWeek.setValueTextSize(0f);
+
+
+        RadarData radarData = new RadarData();
+        radarData.addDataSet(radarDataUserTraits);
+        radarData.addDataSet(radarDataUserTraitsLastWeek);
+
+        String[] labels = {"Power", "Consistency", "Diversity", "Tenacity", "Balance"};
+        radarChart.getDescription().setText("Your Traits");
+        XAxis xAxis = radarChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
+        radarChart.setData(radarData);
+
+
+        BarChart barChart = getActivity().findViewById(R.id.barChart);
+
+        ArrayList<BarEntry>dataVals = new ArrayList<BarEntry>();
+
+        dataVals.add(new BarEntry(0, new float[]{38, 25, 86}));
+        dataVals.add(new BarEntry(1, new float[]{42, 15, 76}));
+        dataVals.add(new BarEntry(2, new float[]{41, 25, 86}));
+        dataVals.add(new BarEntry(3, new float[]{42, 15, 66}));
+        dataVals.add(new BarEntry(4, new float[]{30, 15, 96}));
+        dataVals.add(new BarEntry(5, new float[]{35, 25, 76}));
+        dataVals.add(new BarEntry(6, new float[]{38, 15, 68}));
+
+        String[] xAxisLables = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(xAxisLables));
+
+        int[] colorClassArray = new int[]{Color.rgb(94, 119, 140), Color.rgb(128, 151, 166), Color.rgb(204, 212, 217)};
+
+        BarDataSet barDataSet = new BarDataSet(dataVals, "Duration (min)");
+        barDataSet.setColors(colorClassArray);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(0f);
+        barDataSet.setStackLabels(new String[] {"Gaming", "Sports", "Studying"} );
+
+        BarData barData = new BarData(barDataSet);
+
+        barChart.setFitBars(true);
+        barChart.setData(barData);
+        barChart.getDescription().setText("Category Duration Today");
+        barChart.animateY(1000);
     }
 
 
